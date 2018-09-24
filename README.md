b00cb5f083dc49ab4a28e3aee2b72886981723d1
Ashton Hunter <ashtonhunter3@gmail.com>
2018-09-24 15:42:14 -0400

    Add README

---
+++ README.md
@@ -0,0 +1,11 @@
+# Wilson
+
+Using a Wilson score confidence interval to better sort a list of items by calculating its lower bound, given Bernoulli Parameters.
+
+This NPM package was inspired by [Evan Miller's popular blog post](https://evanmiller.org/how-not-to-sort-by-average-rating.html) on better implementations for star ratings on popular sites.
+
+To better understand this idea, I used research from several other resources, including
+
+- [Reddit blog (Randall from XKCD)](https://redditblog.com/2009/10/15/reddits-new-comment-sorting-system/)
+- [Binomial Proportion Confidnece Intervals, Wikipedia](https://en.wikipedia.org/wiki/Binomial_proportion_confidence_interval#Wilson_score_interval)
+- [This Math Overflow post](https://mathoverflow.net/questions/59933/a-problem-with-lower-bound-of-wilson-score-confidence-interval)
--- wilson.js
+++ wilson.js
@@ -1,20 +1,3 @@
-
-console.log('item 1:' , wilson_lower_bound(5, 100) );
-// console.log('item 2:' , wilson_lower_bound(2, 2) );
-// console.log('item 2:' , wilson_lower_bound(3, 3) );
-// console.log('item 2:' , wilson_lower_bound(4, 4) );
-// console.log('item 2:' , wilson_lower_bound(5, 5) );
-// console.log('item 2:' , wilson_lower_bound(10, 10) );
-// console.log('item 2:' , wilson_lower_bound(100, 100) );
-// console.log('item 2:' , wilson_lower_bound(1000, 1000) );
-// console.log('item 2:' , wilson_lower_bound(40000, 4000) );
-// console.log('item 2:' , wilson_lower_bound(10001, 10001) );
-// console.log('item 2:' , wilson_lower_bound(20, 200) );
-// console.log('item 2:' , wilson_lower_bound(.5, 400) );
-
-
-
-
 // let n be the sample size, and pos be the amount of positive reviews
 // calculates the binomial proportion confidence interval
 // lower bound of a bernoulli parameter
@@ -22,7 +5,7 @@ function wilson_lower_bound(pos, n){
   // calculate p hat
   let p = pos/n;
 
-  // in this case, a z score for this implementation
+  // most rudimentary case, a z score for this implementation for .95 confidence
   let z = 1.96;
 
   let interval = ( p + z * z / (2 * n) - z * Math.sqrt( (p * (1 - p) + z * z / ( 4 * n )) / n)) / (1 + z * z /n);
